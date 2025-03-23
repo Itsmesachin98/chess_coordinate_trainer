959087b14bd131fdaf0847ddba64190be1fbd3bf
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-03-23 10:51:22 +0530

    feat: update coordinates after each correct answer

--- script.js
+++ script.js
@@ -17,31 +17,48 @@ document.addEventListener("DOMContentLoaded", () => {
     let boardRotated = false;
     let isTrainingOn = false;
 
-    // This toggles the visibility of the chess pieces
-    hidePieces.addEventListener("click", () => {
-        if (piecesHidden) {
-            document.querySelectorAll("#chessboard img").forEach((piece) => {
-                piece.style.visibility = "";
+    let currSquare;
+    let time = 0;
+    let score = 0;
+
+    const alpha = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+    function generateCoordinates() {
+        const randomAlpha = Math.floor(Math.random() * 8); // Generates random number between 0 to 7
+        const randomNum = Math.floor(Math.random() * 8) + 1; // Generates random number between 1 to 8
+        const coordinate = `${alpha[randomAlpha]}${randomNum}`;
+        return coordinate;
+    }
+
+    function rebindSquareClicks() {
+        document.querySelectorAll(".square-55d63").forEach((square) => {
+            square.addEventListener("click", (e) => {
+                if (isTrainingOn) {
+                    const squareId = e.target.getAttribute("data-square");
+                    const randomSquare =
+                        document.querySelector(".overlay span").innerText;
+
+                    if (squareId === randomSquare) {
+                        document.querySelector(".overlay span").innerText =
+                            generateCoordinates();
+                    }
+                }
             });
-        } else {
-            document.querySelectorAll("#chessboard img").forEach((piece) => {
-                piece.style.visibility = "hidden";
-            });
-        }
+        });
+    }
 
-        piecesHidden = !piecesHidden;
-    });
+    rebindSquareClicks();
 
-    // This rotates the chessboard
-    rotateBoard.addEventListener("click", () => {
-        if (boardRotated) {
-            chess.orientation("white");
-        } else {
-            chess.orientation("black");
-        }
+    function createOverlay() {
+        const overlay = document.createElement("div");
+        overlay.classList.add("overlay");
 
-        boardRotated = !boardRotated;
-    });
+        const span = document.createElement("span");
+        span.innerText = generateCoordinates();
+
+        overlay.appendChild(span);
+        document.querySelector(".board-container").appendChild(overlay);
+    }
 
     function createScoreXTimer() {
         const scoreXTimerContainer = document.createElement("div");
@@ -85,6 +102,33 @@ document.addEventListener("DOMContentLoaded", () => {
         return container;
     }
 
+    // This toggles the visibility of the chess pieces
+    hidePieces.addEventListener("click", () => {
+        if (piecesHidden) {
+            document.querySelectorAll("#chessboard img").forEach((piece) => {
+                piece.style.visibility = "";
+            });
+        } else {
+            document.querySelectorAll("#chessboard img").forEach((piece) => {
+                piece.style.visibility = "hidden";
+            });
+        }
+
+        piecesHidden = !piecesHidden;
+    });
+
+    // This rotates the chessboard
+    rotateBoard.addEventListener("click", () => {
+        if (boardRotated) {
+            chess.orientation("white");
+        } else {
+            chess.orientation("black");
+        }
+
+        boardRotated = !boardRotated;
+        rebindSquareClicks();
+    });
+
     startBtn.addEventListener("click", () => {
         if (!document.querySelector(".score-x-timer-container")) {
             leftPane.appendChild(createScoreXTimer());
@@ -93,8 +137,10 @@ document.addEventListener("DOMContentLoaded", () => {
         isTrainingOn = !isTrainingOn;
         if (isTrainingOn) {
             startBtn.innerText = "Stop Training";
+            createOverlay();
         } else {
             startBtn.innerText = "Start Training";
+            document.querySelector(".overlay").remove();
         }
     });
 
